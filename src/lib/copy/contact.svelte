cd6e9832c75d47cd751a802c4a0d822164be909c
Wyatt Strother <wstrother@users.noreply.github.com>
2023-07-25 05:23:53 -0400

    about sort of

---
+++ src/lib/copy/contact.svelte
@@ -0,0 +1,5 @@
+<ul>
+    <li>Github</li>
+    <li>LinkedIn</li>
+    <li>Wyatt.Strother@gmail.com</li>
+</ul>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,3 +1,12 @@
+<script>
+	import { TabGroup, Tab } from '@skeletonlabs/skeleton';
+	import AboutMe from '$lib/copy/about-me.svelte';
+	import AboutSite from '$lib/copy/about-site.svelte';
+	import Contact from '$lib/copy/contact.svelte';
+
+	let tabSet = 0;
+</script>
+
 <div class="splash-container">
 	<div class="splash-quote">
 		<h1>
@@ -7,3 +16,31 @@
 		<div>-Slavoj Zizek</div>
 	</div>
 </div>
+
+<div class='page-main'>
+	<TabGroup>
+		<Tab bind:group={tabSet} name="tab1" value={0}>
+			About Wyatt
+		</Tab>
+	
+		<Tab bind:group={tabSet} name="tab2" value={1}>
+			About This Site
+		</Tab>
+		
+		<Tab bind:group={tabSet} name="tab3" value={2}>
+			Contact Info
+		</Tab>
+			
+		<svelte:fragment slot="panel">
+			{#if tabSet === 0}
+				<AboutMe />
+			{:else if tabSet === 1}
+				<AboutSite />
+			{:else if tabSet === 2}
+				<Contact />
+			{/if}
+		</svelte:fragment>
+	</TabGroup>
+</div>
+
+
