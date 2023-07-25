3dc0d390b837dfebf02991541da050e0327bbe57
Wyatt Strother <wstrother@users.noreply.github.com>
2023-07-25 03:27:57 -0400

    my big face

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,11 @@
+<script>
+	import face from '$lib/images/face.png';
+</script>
+
 <a href='/about' >About me</a>
 
 <a href='/portfolio' >Portfolio</a>
+
+<div class='face-container bg-tertiary-900 w-[600px] h-[600px] flex justify-center rounded-full overflow-clip'>
+	<img src={face} alt="Wyatt's Face">
+</div>
