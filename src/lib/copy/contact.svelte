ccf828f7d38f1475c64d8152c2da842b9db49ba6
Wyatt Strother <wstrother@users.noreply.github.com>
2023-07-26 17:11:28 -0400

    contact links

--- src/lib/copy/contact.svelte
+++ src/lib/copy/contact.svelte
@@ -1,5 +1,5 @@
 <ul>
-    <li>Github</li>
-    <li>LinkedIn</li>
+    <li><a href='https://github.com/wstrother' target='_blank'>Github</a></li>
+    <li><a href="https://www.linkedin.com/in/wyatt-strother-754016144/" target='_blank'>LinkedIn</a></li>
     <li>Wyatt.Strother@gmail.com</li>
 </ul>
