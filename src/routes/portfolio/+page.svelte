229589ba44ece3f552e99906528beaa6f55d5835
Wyatt Strother <wstrother@users.noreply.github.com>
2023-07-26 01:23:34 -0400

    formatting for portfolio

--- src/routes/portfolio/+page.svelte
+++ src/routes/portfolio/+page.svelte
@@ -1,1 +1,46 @@
-Portfolio
+<script>
+	import { TabGroup, Tab } from '@skeletonlabs/skeleton';
+	import { fade } from "svelte/transition";
+    import TwitchBot from "$lib/copy/projects/twitch-bot.svelte";
+
+    let tabSet = 0;
+
+</script>
+
+<div class="splash-container" in:fade>
+    <div class="splash-quote mb-5"><h1>project portfolio</h1></div>
+</div>
+
+<div class="page-main">
+    <TabGroup
+		regionList='variant-glass-tertiary rounded'
+		regionPanel='bg-tertiary-900 rounded p-1 min-h-[50vh] m-0'
+		spacing='space'
+	>
+		<Tab bind:group={tabSet} name="tab1" value={0}>
+			TwitchBot
+		</Tab>
+	
+		<Tab bind:group={tabSet} name="tab2" value={1}>
+			Pokemon Battleships
+		</Tab>
+		
+		<Tab bind:group={tabSet} name="tab3" value={2}>
+			Odds and Ends
+		</Tab>
+			
+		<svelte:fragment slot="panel">
+			{#key tabSet}
+				<div in:fade>
+					{#if tabSet === 0}
+						<TwitchBot />
+					{:else if tabSet === 1}
+                        battleships
+					{:else if tabSet === 2}
+                        other stuff
+					{/if}
+				</div>
+			{/key}
+		</svelte:fragment>
+	</TabGroup>
+</div>
