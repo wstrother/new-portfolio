d1e06d4d2e86f3faee2fea0aadb7955501f39f40
Wyatt Strother <wstrother@users.noreply.github.com>
2023-08-06 02:49:44 -0400

    face fade in

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,11 @@
 <script>
 	import face from '$lib/images/face.png';
+	import { fade } from 'svelte/transition';
 </script>
 
 <div class='landing-container'>
 	<div class='face-container'>
-		<img src={face} alt="Wyatt's Face">
+		<img src={face} alt="Wyatt's Face" in:fade>
 	</div>
 	
 	<div class='nav-container'>
