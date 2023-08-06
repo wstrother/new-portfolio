e696bf7bf59f4bbc99d2a6e8c7721fa6f08e0181
Wyatt Strother <wstrother@users.noreply.github.com>
2023-08-06 06:09:56 -0400

    slotted project header

--- src/lib/components/project-header.svelte
+++ src/lib/components/project-header.svelte
@@ -5,18 +5,29 @@
     export let repo;
 </script>
 
-<h1 class='h3 ml-4'>{name}</h1>
 
-<a href={repo} target='_blank' class='btn btn-sm bg-primary-400 p-2 ml-2'>Visit on Github</a>
-
-{#if url}
-    <a href={url} target='_blank' class='btn btn-sm bg-primary-400 p-2'>Visit Website</a>
-{/if}
-
-{#if img}
-    <div class='project-image-container float-left rounded overflow-hidden border border-black w-[50%] m-4'>
-        <a href={url} target='_blank'>
-            <img src={img} alt={`${name} screenshot`} />
-        </a>
+<div class="flex justify-between items-center">
+    <h1 class='h3 ml-4'>{name}</h1>
+    
+    <div class="mr-4">
+        <a href={repo} target='_blank' class='btn btn-sm bg-primary-400 p-2 mr-2'>Visit on Github</a>
+        
+        {#if url}
+            <a href={url} target='_blank' class='btn btn-sm bg-primary-400 p-2'>Visit Website</a>
+        {/if}
     </div>
-{/if}
+</div>
+
+
+<div class="project-main overflow-hidden">
+    
+    {#if img}
+        <div class='project-image-container rounded float-left border border-black w-[40%] m-4'>
+            <a href={url} target='_blank'>
+                <img src={img} alt={`${name} screenshot`} />
+            </a>
+        </div>
+    {/if}
+    
+    <slot></slot>
+</div>
