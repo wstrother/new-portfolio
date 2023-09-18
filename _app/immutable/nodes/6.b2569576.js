0a81512496b3c31740e43d25e21e61cc8d7934c7
Wyatt Strother <wstrother@users.noreply.github.com>
2023-09-18 01:29:44 -0400

    Updates

---
+++ _app/immutable/nodes/6.b2569576.js
@@ -0,0 +1,19 @@
+import{s as r,w as o,x as l,i as p,f as c}from"../chunks/scheduler.101e0d8c.js";import{S as m,i as u,b as d,d as g,m as f,t as h,a as y,e as w}from"../chunks/index.f258eba1.js";import{P as b}from"../chunks/project.e080c6b2.js";const _="/_app/immutable/assets/streamingapp.eed875b4.png";function $(n){let t;return{c(){t=o(`This streaming application is a full stack web app that will provide \r
+some really cool functionality for streaming with dynamic \r
+layouts with real time state management.\r
+\r
+While the project isn't currently deployed, progress is steady and I'll\r
+be posting dev vlogs as I get closer to actually deploying \r
+an alpha version that I will be testing along with some volunteers.\r
+\r
+This project is built in SvelteKit/Typescript with a Postgres/Supabase backend, \r
+and also utilizes the Tailwind CSS library for UI and customization.`)},l(a){t=l(a,`This streaming application is a full stack web app that will provide \r
+some really cool functionality for streaming with dynamic \r
+layouts with real time state management.\r
+\r
+While the project isn't currently deployed, progress is steady and I'll\r
+be posting dev vlogs as I get closer to actually deploying \r
+an alpha version that I will be testing along with some volunteers.\r
+\r
+This project is built in SvelteKit/Typescript with a Postgres/Supabase backend, \r
+and also utilizes the Tailwind CSS library for UI and customization.`)},m(a,e){p(a,t,e)},d(a){a&&c(t)}}}function v(n){let t,a;return t=new b({props:{project:n[0],$$slots:{default:[$]},$$scope:{ctx:n}}}),{c(){d(t.$$.fragment)},l(e){g(t.$$.fragment,e)},m(e,s){f(t,e,s),a=!0},p(e,[s]){const i={};s&2&&(i.$$scope={dirty:s,ctx:e}),t.$set(i)},i(e){a||(h(t.$$.fragment,e),a=!0)},o(e){y(t.$$.fragment,e),a=!1},d(e){w(t,e)}}}function S(n){return[{title:"Streaming App",img:_,repoName:"streaming-app",readmeFile:"master/README.md"}]}class x extends m{constructor(t){super(),u(this,t,S,v,r,{})}}export{x as component};
