f1e046ea7dc9eb554ec2854fd0630432f4fe792d
Wyatt Strother <wstrother@users.noreply.github.com>
2023-09-17 21:07:15 -0400

    now with error route

---
+++ src/routes/+error.svelte
@@ -0,0 +1,14 @@
+<script>
+    import {page} from '$app/stores'
+    console.log($page)
+</script>
+
+<div>
+    <span class="error">{$page.url.pathname}</span> was not found. Sorry about that.
+</div>
+
+<style lang='postcss'>
+    .error {
+        @apply text-2xl;
+    }
+</style>
