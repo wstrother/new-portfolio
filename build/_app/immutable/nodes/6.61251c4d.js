dd8273dce5f808327d70e3cae3e9a760fd3e8f5e
Wyatt Strother <wstrother@users.noreply.github.com>
2024-05-12 21:58:37 -0400

    track build output

---
+++ build/_app/immutable/nodes/6.61251c4d.js
@@ -0,0 +1,19 @@
+import{s as r,t as o,b as l,i as p,d as c}from"../chunks/scheduler.bcceadc5.js";import{S as m,i as g,c as u,a as d,m as f,t as h,b as y,d as b}from"../chunks/index.0aa9e676.js";import{P as _}from"../chunks/project.7fa38e6c.js";const w="/_app/immutable/assets/streaming-app-1.e28b9ecf.png",$="/_app/immutable/assets/streaming-app-2.a04f80c0.png",v="/_app/immutable/assets/streaming-app-3.3ebf3b13.gif";function S(n){let t;return{c(){t=o(`This streaming application is a full stack web app that will provide \r
+some really cool functionality for streaming with dynamic \r
+layouts with real time state management.\r
+\r
+While the project isn't currently deployed, progress is steady and I'll\r
+be posting dev vlogs as I get closer to actually deploying \r
+an alpha version that I will be testing along with some volunteers.\r
+\r
+This project is built in SvelteKit/Typescript with a Postgres/Supabase backend, \r
+and also utilizes the Tailwind CSS library for UI and customization.`)},l(a){t=l(a,`This streaming application is a full stack web app that will provide \r
+some really cool functionality for streaming with dynamic \r
+layouts with real time state management.\r
+\r
+While the project isn't currently deployed, progress is steady and I'll\r
+be posting dev vlogs as I get closer to actually deploying \r
+an alpha version that I will be testing along with some volunteers.\r
+\r
+This project is built in SvelteKit/Typescript with a Postgres/Supabase backend, \r
+and also utilizes the Tailwind CSS library for UI and customization.`)},m(a,e){p(a,t,e)},d(a){a&&c(t)}}}function j(n){let t,a;return t=new _({props:{project:n[0],$$slots:{default:[S]},$$scope:{ctx:n}}}),{c(){u(t.$$.fragment)},l(e){d(t.$$.fragment,e)},m(e,s){f(t,e,s),a=!0},p(e,[s]){const i={};s&2&&(i.$$scope={dirty:s,ctx:e}),t.$set(i)},i(e){a||(h(t.$$.fragment,e),a=!0)},o(e){y(t.$$.fragment,e),a=!1},d(e){b(t,e)}}}function I(n){return[{title:"Streaming App",repoName:"streaming-app",readmeFile:"master/README.md",imgs:[w,$,v]}]}class x extends m{constructor(t){super(),g(this,t,I,j,r,{})}}export{x as component};
---
+++ build/_app/immutable/nodes/7.acc5609a.js
@@ -0,0 +1,23 @@
+import{s,t as i,b as c,i as h,d as m}from"../chunks/scheduler.bcceadc5.js";import{S as p,i as l,c as d,a as u,m as f,t as b,b as g,d as y}from"../chunks/index.0aa9e676.js";import{P as w}from"../chunks/project.7fa38e6c.js";const _="/_app/immutable/assets/twitchbot.36c76a70.png";function $(a){let t;return{c(){t=i(`This code repository defines a simple Python script which will launch \r
+a helpful bot to connect to your Twitch chat.\r
+\r
+The real fun comes from the way this project defines and implements a specific JSON spec for creating a set of commands. These commands can be as simple\r
+as pasting a link to your Youtube in the chat, but they get way, way, way more sophisticated.\r
+\r
+Commands can implement conditional branching. They can read or write to arbitrary state variables. They can send requests to\r
+and receive data from external web APIs. And they can be chained together such that the output of one command informs the input of another.\r
+\r
+When I stream on Twitch, I also use my bot to control a local node application that controls graphical layout components on my stream,\r
+and the next big step in this project is going to be expanding on this idea with an actual web platform and graphical UI, so be on the look out\r
+for more big updates related to this project.`)},l(o){t=c(o,`This code repository defines a simple Python script which will launch \r
+a helpful bot to connect to your Twitch chat.\r
+\r
+The real fun comes from the way this project defines and implements a specific JSON spec for creating a set of commands. These commands can be as simple\r
+as pasting a link to your Youtube in the chat, but they get way, way, way more sophisticated.\r
+\r
+Commands can implement conditional branching. They can read or write to arbitrary state variables. They can send requests to\r
+and receive data from external web APIs. And they can be chained together such that the output of one command informs the input of another.\r
+\r
+When I stream on Twitch, I also use my bot to control a local node application that controls graphical layout components on my stream,\r
+and the next big step in this project is going to be expanding on this idea with an actual web platform and graphical UI, so be on the look out\r
+for more big updates related to this project.`)},m(o,e){h(o,t,e)},d(o){o&&m(t)}}}function T(a){let t,o;return t=new w({props:{project:a[0],$$slots:{default:[$]},$$scope:{ctx:a}}}),{c(){d(t.$$.fragment)},l(e){u(t.$$.fragment,e)},m(e,n){f(t,e,n),o=!0},p(e,[n]){const r={};n&2&&(r.$$scope={dirty:n,ctx:e}),t.$set(r)},i(e){o||(b(t.$$.fragment,e),o=!0)},o(e){g(t.$$.fragment,e),o=!1},d(e){y(t,e)}}}function j(a){return[{title:"Twitch Bot",imgs:[_],readmeFile:"master/readme.md",repoName:"ahp_twitch_bot",siteURL:"https://wstrother.github.io/ahp_twitch_bot_site/"}]}class v extends p{constructor(t){super(),l(this,t,j,T,s,{})}}export{v as component};
